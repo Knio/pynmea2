c56717b5e859e978ad3b52b8f826faa5d50489f8
Simon Howroyd <howroydlsu@gmail.com>
2024-01-29 14:05:19 +0000

    Removed depreciated `imp` and replaced with `importlib`

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
+import importlib.machinery
+import importlib.util
+
 from setuptools import setup
 
-import imp
-_version = imp.load_source("pynmea2._version", "pynmea2/_version.py")
+
+def load_source(modname, filename):
+    """Load a source file and return its module object.
+
+    From: https://docs.python.org/3.12/whatsnew/3.12.html#imp
+    """
+    loader = importlib.machinery.SourceFileLoader(modname, filename)
+    spec = importlib.util.spec_from_file_location(
+        modname, filename, loader=loader)
+    module = importlib.util.module_from_spec(spec)
+    # The module is always executed and not cached in sys.modules.
+    # Uncomment the following line to cache the module.
+    # sys.modules[module.__name__] = module
+    loader.exec_module(module)
+    return module
+
+
+_version = load_source("pynmea2._version", "pynmea2/_version.py")
 
 setup(
     name='pynmea2',
@@ -12,7 +31,7 @@ setup(
     url='https://github.com/Knio/pynmea2',
 
     description='Python library for the NMEA 0183 protcol',
-    packages=['pynmea2','pynmea2.types','pynmea2.types.proprietary'],
+    packages=['pynmea2', 'pynmea2.types', 'pynmea2.types.proprietary'],
     keywords='python nmea gps parse parsing nmea0183 0183',
 
     classifiers=[
