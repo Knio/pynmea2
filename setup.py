802bfb627eb05a1ee655854dd261800061322b9a
Simon H <howroydlsu@gmail.com>
2024-02-11 17:59:59 +0000

    Python 3.12 compatibility (#164)
    * Removed depreciated `imp` and replaced with `importlib`
    
    * Updated meta classifiers to include newer Python versions
    
    * Added Python3.12 into github workflow action
    
    * Updated workflow to test all versions we say we do
    
    * Tidied mixed markup styles
    
    * Assed Windows and MacOS to tests
    
    * Updated depreciated setup-python action to v5
    
    * Adding Python3.12 to Action
    
    * Removed my test branch from Actions

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
+import importlib.machinery
+import importlib.util
+
 from setuptools import setup
 
-import imp
-_version = imp.load_source("pynmea2._version", "pynmea2/_version.py")
+
+def load_source(modname, filename):
+    """Load a source file and return its module object.
+
+    From: https://docs.python.org/3.12/whatsnew/3.12.html#imp
+    """
+    loader = importlib.machinery.SourceFileLoader(modname, filename)
+    spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
+    module = importlib.util.module_from_spec(spec)
+    # The module is always executed and not cached in sys.modules.
+    # Uncomment the following line to cache the module.
+    # sys.modules[module.__name__] = module
+    loader.exec_module(module)
+    return module
+
+_version = load_source("pynmea2._version", "pynmea2/_version.py")
 
 setup(
     name='pynmea2',
@@ -29,6 +46,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Software Development :: Libraries :: Python Modules',
